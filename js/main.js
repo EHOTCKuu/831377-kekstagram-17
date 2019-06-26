62ce9940dd355ef424d133641343afc300cf5926
EHOTCKuu <poluyano@yandex.ru>
2019-06-26 15:24:54 +0300

    Добавляем фильтры и масштабирование

--- js/main.js
+++ js/main.js
@@ -8,7 +8,85 @@ var AMOUNT_AVATARS_MIN = 1;
 var AMOUNT_IMAGES_MAX = 25;
 var COMMENTS_MIN = 0;
 var COMMENTS_MAX = 9;
+var ESC_KEYCODE = 27;
+var EFFECTS = ['effects__preview--none', 'effects__preview--chrome', 'effects__preview--sepia', 'effects__preview--marvin', 'effects__preview--phobos', 'effects__preview--heat'];
+var FILTERS = [' none', ' grayscale(0..1)', ' sepia(0..1)', ' invert(0..100%)', ' blur(0..3px)', ' filter: brightness(1..3)'];
+var SCALE_STEP = 25;
+var SCALE = 100;
 
+// привязываем эффект к Radiobutton
+var uploadPreview = document.querySelector('.img-upload__preview');
+var radiobuttonEffects = document.querySelectorAll('.effects__radio');
+
+var clickHandler = function (effect, color, filters) {
+  effect.addEventListener('click', function () {
+    uploadPreview.classList = ['img-upload__preview'];
+    if (filters !== ' none') {
+      uploadPreview.classList.add(color);
+      uploadPreview.style.filter = filters;
+    }
+  });
+};
+
+for (var effect = 0; effect < radiobuttonEffects.length; effect++) {
+  clickHandler(radiobuttonEffects[effect], EFFECTS[effect], FILTERS[effect]);
+}
+
+// Открытие попапа и закрытие попапа по крестику и эскейпу
+var overlayForm = document.querySelector('.img-upload__overlay');
+var cancelForm = document.querySelector('.img-upload__cancel');
+var uploadFile = document.querySelector('#upload-file');
+
+var onPopupEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  overlayForm.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function () {
+  overlayForm.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+uploadFile.addEventListener('change', function () {
+  openPopup();
+
+});
+
+cancelForm.addEventListener('click', function () {
+  closePopup();
+});
+
+// Масштабирование картинки
+var controlSmaller = document.querySelector('.scale__control--smaller');
+var controlBigger = document.querySelector('.scale__control--bigger');
+var controlValue = document.querySelector('.scale__control--value');
+
+var getchangeScale = function (step, value) {
+  var i = step + value;
+  if (i >= 25 && i <= 100) {
+    return i;
+  }
+  return step;
+};
+
+var getControl = function (scale, steps) {
+  scale.addEventListener('click', function () {
+    SCALE = getchangeScale(SCALE, steps);
+    controlValue.value = SCALE + '%';
+    var scaleInfo = 'transform: SCALE(' + SCALE / 100 + ')';
+    uploadPreview.style = scaleInfo;
+  });
+};
+getControl(controlSmaller, -(SCALE_STEP));
+getControl(controlBigger, SCALE_STEP);
+
+// Создание пользователей, комментов, лайков.
 var imageList = document.querySelector('.pictures');
 var imageTemplate = document.querySelector('#picture')
     .content
