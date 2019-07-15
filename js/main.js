ee6a6889f2bdb659253e282282470b521646c5a9
EHOTCKuu <poluyano@yandex.ru>
2019-07-15 13:06:46 +0300

    Максимум подвижности

--- js/main.js
+++ js/main.js
@@ -231,3 +231,45 @@ commentText.addEventListener('focus', function () {
 commentText.addEventListener('blur', function () {
   document.addEventListener('keydown', onPopupEscPress);
 }); // удаляем функцию назначения закрытия попапа при потере фокуса с формы комментария.
+
+
+// =============
+
+(function () {
+  levelPin.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = levelPin.getBoundingClientRect().left + levelPin.offsetWidth / 2;
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = startCoords - moveEvt.clientX;
+
+      startCoords = moveEvt.clientX;
+
+      var pinLeft = levelPin.offsetLeft - shift;
+
+      var lineLeft = levelLine.getBoundingClientRect().left;
+      var lineRight = levelLine.getBoundingClientRect().right;
+      if (startCoords <= lineLeft) {
+        pinLeft = 0;
+      } else if (startCoords >= lineRight) {
+        pinLeft = levelLine.clientWidth;
+      }
+
+      levelPin.style.left = pinLeft + 'px';
+      levelDepth.style.width = pinLeft + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+})();
